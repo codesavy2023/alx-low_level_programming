f7caaaded634047b2cc566762e41851a697b3f99
Augustus Enabunlele <augustusenabunlele@yahoo.com>
2023-03-16 12:08:18 -0700

    size of ..

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ char a;
 int b;
 long int c;
 long long int d;
-float f;
+float e;
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
