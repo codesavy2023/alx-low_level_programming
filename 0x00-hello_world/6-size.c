79fe361877f9ec39053ef7342c61cb205e86b5f3
Augustus Enabunlele <augustusenabunlele@yahoo.com>
2023-03-16 12:06:13 -0700

    size of ..

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int b;
 long int c;
 long long int d;
 float f;
-print f("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-print f("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
-print f("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-print f("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-print f("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
 return (0);
 }
