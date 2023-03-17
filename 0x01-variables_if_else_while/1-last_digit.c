538f480d51340746b272978e5ed89934fbee3451
Augustus Enabunlele <augustusenabunlele@yahoo.com>
2023-03-17 06:24:24 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ lastd = n % 10;
 
 if (lastd > 5)
 {
-printf("last digit of %d is %d and is greater than 5\n", n, lastd);
+printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
 }
 
 else if (lastd == 0)
